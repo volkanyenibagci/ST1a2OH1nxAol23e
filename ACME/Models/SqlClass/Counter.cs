63a97d253031e2b302d53d568a46176b0af8ab13
279908@ogr.ktu.edu.tr <279908@ogr.ktu.edu.tr>
2020-09-12 22:03:08 +0300

    acme

---
+++ ACME/Models/SqlClass/Counter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+namespace ACME.Models.SqlClass
+{
+	[Table("tbl_Counter")]
+	public class Counter
+	{
+		[Key, DatabaseGenerated(DatabaseGeneratedOption.Identity), Required]
+		public int ID { get; set; }
+
+		
+		public int Hour { get; set; }
+
+
+		public int Mınute { get; set; }
+
+		public int Second { get; set; }
+
+
+
+
+
+	}
+}
